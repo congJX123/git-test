a32c2337a46081e0d789e7722025776209065cde
congJX123 <142172541+congJX123@users.noreply.github.com>
2023-08-14 18:31:47 +0800

    pull test

--- src/main/java/com/gongsi/git/GitTest.java
+++ src/main/java/com/gongsi/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix commit2");
         System.out.println("master commit2");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
